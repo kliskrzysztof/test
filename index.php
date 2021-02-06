103aef4ba6ddcc219fe82d2ae0992d7c0c8e926d
kliskrzysztof <kliskrzysztof1@gmail.com>
2021-02-06 15:44:12 +0100

    Test próba

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+	<title>nauka Java Script</title>
+	<meta charset="utf-8">
+	<style type="text/css">
+		html,body
+		{
+			background-color: yellow;
+		}
+	</style>
+</head>
+<body>
+	
+	<!--<pre>
+	<script type="text/javascript" defer="yes">
+		document.writeln("aaaa         ffff");//asfdsadfads
+
+		document.writeln("bbb");/*asdfas*/
+
+	</script>
+</pre>-->
+	<!--<script type="text/javascript" defer="yes">
+		document.writeln("<b>kasJCdckjas"+"<b>");
+		document.write("aaaaaaa"+"bbbbb</b>");
+	</script>
+	<script type="text/javascript" defer="yes" async="no">/*komentarz*/
+		/*document.writeln("mój piewrwszy program<br>"+"aaaaa"+"<b>");
+		document.write("mój piewrwszy program");
+		alert("uawga do nauki 	!!!");
+	confirm("Czy chcesz sie uczyć ");*/
+	var imie=	prompt	("Podaj jak się nazywasz");
+	var ocena=prompt	("Podaj jaką chcesz ocene","ndst");
+document.writeln("<br>chcesz ocene "+ocena+" twoje imie to "+imie);
+var
+
+
+	</script>-->
+	<!--<script type="text/javascript" defer="yes">
+		var pierwsza_liczba="34";
+		var druga_liczba="15";
+
+		var suma="pierwsza_liczba"+"druga_liczba";
+
+		var suma1=pierwsza_liczba+druga_liczba;
+		document.writeln("Twój piewrwszy wynik     "+suma);
+		document.writeln("<br>Twój drugi wynik     "+suma1);
+
+
+	</script>-->
+
+	<script type="text/javascript" defer="yes" async="no">
+		//var pierwsza_liczba=14;
+		/*var druga_liczba=15;
+
+		var suma="pierwsza_liczba"+"druga_liczba";*/
+
+		//var suma1=pierwsza_liczba+druga_liczba;
+		/*document.writeln("Twój piewrwszy wynik     "+suma);
+		document.writeln("<br>Twój drugi wynik     "+suma1);
+		document.writeln("<br>Twój pierwsza liczba to "+pierwsza_liczba+" Twoja druga_liczba to "+druga_liczba+" Suma twoich liczb to"+suma1);*/
+
+<pre>
+	</script>
+	<!--zmienne w Java Script-->
+	<script type="text/javascript" defer="yes">
+		//var działa w  w dół
+	document.writeln(" na górze Dziś mamy dzień "+dzien+"<br>");
+		var dzien="wtorek";
+		document.writeln(" na dole Dziś mamy dzień "+dzien);
+		var dzien="sroda";
+		document.writeln(" na dole nowy ziś mamy dzień "+dzien);
+		let sampoczucje="dobre";
+	document.writeln(" <br>na dole Dziś masz "+sampoczucje);
+
+	let sampoczucje1=" bardzo dobre";
+	document.writeln(" <br>na dole Dziś masz nowe "+sampoczucje);
+		const stala=10; //definjuje stałą wartość ona sie nie bedzie zmieniac
+
+
+	</script>
+	
+</pre>
+</body>
+</html>
